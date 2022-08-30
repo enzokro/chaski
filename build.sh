c6f4f04f4ba26c54af6723767f64d0c439b47cd0
enzokro <christopher.kroenke@gmail.com>
2022-08-30 18:02:42 +0000

    add cleanup to build script to force changes

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# clean out the old library and site
+if [[ -d chaski ]]; then
+  rm -Rf chaski
+fi
+if [[ -d _docs ]]; then
+  rm -Rf _docs
+fi
+
 # run library building and site-deploying commands
 nbdev_prepare
 nbdev_quarto
